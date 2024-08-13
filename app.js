fd6cce3dba0e4227b95cda44e9deb5d63ade99db
AbeysiriLasantha <lasantha.abeysisri@gmail.com>
2024-08-13 15:28:08 -0500

    Creating adventurer object and loop through the inventory array.

---
+++ app.js
@@ -0,0 +1,21 @@
+
+//Part 1: Humble Beginnings
+// a) Creating adventurer “Robin.”
+
+    const adventurer = {
+        name: "Robin",
+        health: 10,
+        inventory: ["sword", "potion", "artifact"]
+        }
+
+// b) Loop through adventurer Robin's inventory.
+
+    const invLength = adventurer.inventory.length;
+
+        for (let i=0; i<invLength; i++) {
+            console.log(adventurer.inventory[i]);
+        }
+
+
+        
+
