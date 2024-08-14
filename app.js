dc13897d1d2274a7d97cefc89863ee827bd3a038
AbeysiriLasantha <lasantha.abeysisri@gmail.com>
2024-08-13 23:17:24 -0500

    Creating Adventurer, Companion classes and creating Robin, Leo and Frank objects.

--- app.js
+++ app.js
@@ -77,12 +77,76 @@
         }
     }
 
-    const robin = new Character("Robin");
-    robin.inventory = ["sword", "potion", "artifact"];
-    robin.companion = new Character("Leo");
-    robin.companion.type = "Cat";
-    robin.companion.companion = new Character("Frank");
-    robin.companion.companion.type = "Flea";
-    robin.companion.companion.inventory = ["small hat", "sunglasses"];
+    // const robin = new Character("Robin");
+    // robin.inventory = ["sword", "potion", "artifact"];
+    // robin.companion = new Character("Leo");
+    // robin.companion.type = "Cat";
+    // robin.companion.companion = new Character("Frank");
+    // robin.companion.companion.type = "Flea";
+    // robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
-    robin.companion.roll();
+    // robin.companion.roll();
+
+    
+//=======================
+//Part 03: Class Features
+//=======================
+
+
+// g) Making the class Character and adding data to that.
+
+    class Adventurer extends Character {
+        constructor (name, role) {
+        super(name);
+
+        // Adventurers have specialized roles.
+        this.role = role;
+
+        // Every adventurer starts with a bed and 50 gold coins.
+        this.inventory.push("bedroll", "50 gold coins");
+        }
+
+        // Adventurers have the ability to scout ahead of them.
+        scout () {
+        console.log(`${this.name} is scouting ahead...`);
+        super.roll();
+        }
+    }
+
+
+// f) Creating class Companion.
+
+    class Companion extends Character {
+        constructor (name, type, belongings) {
+            super(name);
+            this.type = type;
+            this.belongings = belongings;
+        }
+    }
+
+// g) Change the declaration of Robin.
+    console.log ("Creating Robin");
+    console.log ("==============");
+    
+    const robin = new Adventurer ("Robin", "Leader");
+    console.log (robin);
+
+
+// h) Change the declaration of Leo.
+    console.log ("Creating Leo");
+    console.log ("============");
+
+    const leo = new Companion ("Leo", "Cat");
+    console.log (leo);
+
+// i) Change the declaration of Frank.
+    console.log ("Creating Frank");
+    console.log ("==============");
+
+    const frank = new Companion ("Frank", "Flea", ["small hat", "sunglasses"]);
+    console.log (frank);
+
+ 
+
+
+
